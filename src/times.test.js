b6a901391454d2186fcccdbda0e411534125e1f6
Jonas <jonasexplore@gmail.com>
2023-02-27 20:57:43 -0300

    test: fix test

--- src/times.test.js
+++ src/times.test.js
@@ -2,5 +2,5 @@ const { times } = require("./times");
 const { test, expect } = require("@jest/globals");
 
 test("adds 1 * 2 to equal 2", () => {
-  expect(times(1, 2)).toBe(3);
+  expect(times(1, 2)).toBe(2);
 });
