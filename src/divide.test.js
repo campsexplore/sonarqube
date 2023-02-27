86eea67033a7a226e978ca164589b8cda08ff51d
Jonas <jonasexplore@gmail.com>
2023-02-27 20:52:35 -0300

    test: coverage improve and create src folder

--- src/divide.test.js
+++ src/divide.test.js
@@ -4,3 +4,7 @@ const { test, expect } = require("@jest/globals");
 test("divides 1 / 2 to equal 0.5", () => {
   expect(divide(1, 2)).toBe(0.5);
 });
+
+test("divides 2 / 0 to equal 0", () => {
+  expect(divide(1, 0)).toBe(0);
+});
